1c78db002cbcfd1c72fdb40e928dedb7ecd51a1c
Alex Xie <alexieyizhe@gmail.com>
2018-02-20 21:29:28 -0500

    added abilities to create, edit, delete articles, updated mvc

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,37 @@
+class ArticlesController < ApplicationController
+	include ArticlesHelper
+	def index
+		@articles = Article.all
+	end
+
+	def show
+		@article = Article.find(params[:id])
+	end
+
+	def new
+		@article = Article.new
+	end
+	def create
+		@article = Article.new(article_params)
+		@article.save
+		flash.notice = "Article: '#{@article.title}' was created!"
+		redirect_to article_path(@article)
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+	def update
+		@article = Article.find(params[:id])
+		@article.update(article_params)
+		flash.notice = "Article: '#{@article.title}' was updated!"
+		redirect_to article_path(@article)
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		flash.notice = "Article: '#{@article.title}' was deleted!"
+		redirect_to articles_path
+	end
+end
