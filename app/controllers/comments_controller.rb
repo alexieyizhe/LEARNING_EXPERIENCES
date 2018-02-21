14bb31470eea97b3b3eff90df4435102d005d720
Alex Xie <alexieyizhe@gmail.com>
2018-02-20 21:58:52 -0500

    added ability to comment, minor improvements

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,10 @@
+class CommentsController < ApplicationController
+	include CommentsHelper
+	def create
+		@comment = Comment.new(comment_params)
+		@comment.article_id = params[:article_id]
+		@comment.save
+		flash.notice = "Your comment on '#{@comment.article.title}' was created!"
+		redirect_to article_path(@comment.article)
+	end
+end
